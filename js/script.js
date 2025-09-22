cb6c45b6c5cc300388a1766e762b6433a9d260cc
denglan <18030893186@163.com>
2025-09-22 22:19:08 +0800

    feee

--- js/script.js
+++ js/script.js
@@ -12,3 +12,30 @@
 // }
 
 // replaceAnswerDescriptionSpans();
+
+document
+  .querySelectorAll(".multi-choice-item:not(.correct-hidden)")
+  .forEach((item) => item.remove());
+
+function findDeepestDescendant(element) {
+  // 如果没有子元素，返回自身
+  if (!element.children || element.children.length === 0) {
+    return element;
+  }
+  // 递归查找第一个子元素的最深后代
+  return findDeepestDescendant(element.children[0]);
+}
+
+document.querySelectorAll(".item[data-index]").forEach((ele) => {
+  let f = false;
+  ele.querySelectorAll(".answer-description>*").forEach((item) => {
+    if (f) return item.remove();
+
+    const deepest = findDeepestDescendant(item);
+    console.log(deepest.innerText);
+    if (deepest.innerText === "-----------------") {
+      f = true;
+      item.remove();
+    }
+  });
+});
